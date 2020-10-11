888937cd939ab1f6f6092085a0e53ee9ddbeb42b
Igor Lyulev <igor.lyulev@stuff-fibre.co.nz>
2020-10-12 12:13:36 +1300

    allow time.Time operations

--- json-time.go
+++ json-time.go
@@ -6,11 +6,11 @@ import (
 )
 
 // JSONTime converts a time from a different string formats to time.Time
-type JSONTime time.Time
+type JSONTime struct{ time.Time }
 
 // MarshalJSON outputs JSON.
 func (d JSONTime) MarshalJSON() ([]byte, error) {
-	return []byte("\"" + time.Time(d).Format(time.RFC3339) + "\""), nil
+	return []byte("\"" + d.Format(time.RFC3339) + "\""), nil
 }
 
 // UnmarshalJSON handles incoming JSON.
@@ -19,14 +19,14 @@ func (d *JSONTime) UnmarshalJSON(b []byte) (err error) {
 	//attempt 1 - RFC3339 format
 	t, err := time.Parse(time.RFC3339, s)
 	if err == nil {
-		*d = JSONTime(t)
+		*d = JSONTime{t}
 		return
 	}
 
 	//attempt 2 - datetime with milliseconds format
 	t, err = time.Parse("2006-01-02T15:04:05.000", s)
 	if err == nil {
-		*d = JSONTime(t)
+		*d = JSONTime{t}
 		return
 	}
 	return fmt.Errorf("No suitable format found for a string %s", s)
